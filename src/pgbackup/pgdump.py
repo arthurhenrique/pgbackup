8a48dab947e2bfd8f6df040c7a7432a99057998f
Arthur Henrique <arthur393@gmail.com>
2019-04-26 21:28:20 +0000

    tests with dumo file

--- src/pgbackup/pgdump.py
+++ src/pgbackup/pgdump.py
@@ -1,5 +1,5 @@
-import subprocess
 import sys
+import subprocess
 
 def dump(url):
     try:
--- tests/test_pgdump.py
+++ tests/test_pgdump.py
@@ -3,7 +3,7 @@ import subprocess
 
 from pgbackup import pgdump
 
-url = "postgres://bob:password@example.com:5432/db_one"
+url = "postgres://root:root@localhost:80/sample"
 
 def test_dump_calls_pg_dump(mocker):
     """
@@ -21,6 +21,4 @@ def test_dump_handles_oserror(mocker):
     with pytest.raises(SystemExit):
         pgdump.dump(url)
 
-    assert pgdump.dump(url)
-    subprocess.Popen.assert_called_with(['pg_dump', url], stdout=subprocess.PIPE)
 
